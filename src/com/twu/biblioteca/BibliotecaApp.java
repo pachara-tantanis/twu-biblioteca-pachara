d9af14f2bcf13c71f5deebdcf09006e71386d503
Pachara Tantanis <pacharatantanis@Pacharas-MacBook-Pro.local>
2019-10-11 16:21:42 +0800

    Add welcome message test

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -1,8 +1,9 @@
 package com.twu.biblioteca;
 
+
 public class BibliotecaApp {
 
+    public static String recentMessage = "";
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,13 +1,37 @@
 package com.twu.biblioteca;
 
 
+import jdk.nashorn.internal.objects.annotations.Function;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class BibliotecaAppTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
 
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void shouldHaveWelcomeMessage() {
+        BibliotecaApp.main(new String[] {});
+        assertEquals("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!", BibliotecaApp.recentMessage);
     }
 }
