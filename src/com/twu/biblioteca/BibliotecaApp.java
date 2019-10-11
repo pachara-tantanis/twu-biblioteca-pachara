d10361f2d920e3872d1c27267013691f2646e439
Pachara Tantanis <pacharatantanis@Pacharas-MacBook-Pro.local>
2019-10-11 16:27:26 +0800

    Add print function

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -6,4 +6,9 @@ public class BibliotecaApp {
     public static String recentMessage = "";
     public static void main(String[] args) {
     }
+
+    public static String println(String message) {
+        recentMessage = message;
+        System.out.println(message);
+    }
 }
