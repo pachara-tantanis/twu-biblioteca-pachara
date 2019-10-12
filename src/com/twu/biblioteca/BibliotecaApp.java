b999dea841c230f5e1a4ca45ee77860d9150621f
Pachara Tantanis <pacharatantanis@Pacharas-MacBook-Pro.local>
2019-10-12 17:20:10 +0700

    extract welcome message ti fucntion

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,6 +4,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
+        showWelcomeMessge();
+    }
+
+    private static void showWelcomeMessge() {
         System.out.println("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
     }
 
