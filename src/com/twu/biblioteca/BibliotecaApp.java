1e3463b5eb0e8d0cb150f81ce2c3bb31508bf0c8
Pachara Tantanis <pacharatantanis@Pacharas-MacBook-Pro.local>
2019-10-12 17:21:34 +0700

    fix typo

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,10 +4,10 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        showWelcomeMessge();
+        showWelcomeMessage();
     }
 
-    private static void showWelcomeMessge() {
+    private static void showWelcomeMessage() {
         System.out.println("Welcome to Biblioteca. You one-stop-shop for great book titles in Bangalore!");
     }
 
