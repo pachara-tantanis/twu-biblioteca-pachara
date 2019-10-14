b25705ed260b700a6987c7ceeca5f0d5a8aa9385
Pachara <pachara.tantanis@thoughtworks.com>
2019-10-14 10:49:02 +0700

    Rename variable

--- src/com/twu/biblioteca/BooksManager.java
+++ src/com/twu/biblioteca/BooksManager.java
@@ -5,24 +5,24 @@ import java.util.List;
 
 public class BooksManager {
 
-    private List<String> listsOfBooks;
+    private List<String> listOfBooks;
     private List<String> listOfCheckedOutBook;
 
     public BooksManager(List<String> listOfBooks) {
-        this.listsOfBooks = listOfBooks;
+        this.listOfBooks = listOfBooks;
         this.listOfCheckedOutBook = new ArrayList<String>();
     }
 
     public List<String> getBooksList() {
-        return this.listsOfBooks;
+        return this.listOfBooks;
     }
 
     public String checkOutBook(String bookName) {
-        for (int i = 0; i < this.listsOfBooks.size(); i++) {
-            String[] bookInfo = this.listsOfBooks.get(i).split(",");
+        for (int i = 0; i < this.listOfBooks.size(); i++) {
+            String[] bookInfo = this.listOfBooks.get(i).split(",");
 
             if (bookInfo[0].equals(bookName)) {
-                String checkedOutBook = this.listsOfBooks.remove(i);
+                String checkedOutBook = this.listOfBooks.remove(i);
                 this.listOfCheckedOutBook.add(checkedOutBook);
                 return "Thank you! Enjoy the book";
             }
@@ -36,7 +36,7 @@ public class BooksManager {
 
             if (bookInfo[0].equals(bookName)) {
                 String returnBook = this.listOfCheckedOutBook.remove(i);
-                this.listsOfBooks.add(returnBook);
+                this.listOfBooks.add(returnBook);
                 return "Thank you for returning the book";
             }
         }
